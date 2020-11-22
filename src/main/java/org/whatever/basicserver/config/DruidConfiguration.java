8bf5247e707887d20ff6fe93e4120a2bd76de451
idideads <idideads@gmail.com>
2020-11-22 17:06:57 +0800

    add druid watcher

--- src/main/java/org/whatever/basicserver/config/DruidConfiguration.java
+++ src/main/java/org/whatever/basicserver/config/DruidConfiguration.java
@@ -1,19 +1,47 @@
 package org.whatever.basicserver.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.Filter;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class DruidConfiguration {
 
-//    @ConfigurationProperties(prefix = "spring.datasource")
     @ConfigurationProperties(prefix = "spring.datasource")
     @Bean
     public DataSource druid(){
         return new DruidDataSource();
+    }
+
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(), "/druid/*");
+        Map<String, String> initPrams = new HashMap<>();
+        initPrams.put("loginUsername", "admin");
+        initPrams.put("loginPassword", "123456");
+        initPrams.put("allow", "");
+
+        servletRegistrationBean.setInitParameters(initPrams);
+        return servletRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
+        filterFilterRegistrationBean.setFilter(new WebStatFilter());
+        Map<String, String> initParms = new HashMap<>();
+        initParms.put("exclusions", "*.js,*.css,/druid/*");
+        filterFilterRegistrationBean.setInitParameters(initParms);
+        return filterFilterRegistrationBean;
     }
 }
