1444e3e556ed73d05c37f10ba83c218723cd9451
idideads <idideads@gmail.com>
2020-11-22 16:33:09 +0800

    init commit

---
+++ src/main/java/org/whatever/basicserver/BasicserverApplication.java
@@ -0,0 +1,13 @@
+package org.whatever.basicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/whatever/basicserver/BasicserverApplicationTests.java
@@ -0,0 +1,25 @@
+package org.whatever.basicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest
+class BasicserverApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection conn = dataSource.getConnection();
+        System.out.println(conn);
+        conn.close();
+    }
+
+}
